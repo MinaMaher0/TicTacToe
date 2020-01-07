27e9da219d42b2e93d4e1b0281edc1c653f5d36b
MinaMaher0 <minamaher30697@gmail.com>
2020-01-07 16:29:02 +0200

    add Request enum

---
+++ src/utils/Request.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+/**
+ *
+ * @author Me5a
+ */
+public enum Request{
+    LOGIN,
+    LOGIN_SUCCESS,
+    LOGIN_FAILED,
+    SIGNUP,
+    SIGN_UP_SUCCESS,
+    SIGN_UP_FAILED,
+}
